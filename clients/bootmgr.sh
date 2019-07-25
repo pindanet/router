405889f7263b419bac17eba359494abe71afcf88
Dany Pinoy <dany.pinoy@pindanet.be>
2019-07-25 15:11:47 +0200

    Update bootmgr.sh

--- clients/bootmgr.sh
+++ clients/bootmgr.sh
@@ -6,3 +6,22 @@ mount /dev/sr0 /mnt/cdrom
 mkdir /mnt/esp
 mount /dev/sda1 /mnt/esp/
 cp -a /mnt/cdrom/sysresccd /mnt/esp/
+UUID=$(lsblk -o partlabel,uuid | grep "EFI system partition" | awk '{print $4}')
+cat > /etc/grub.d/25_sysresccd <<EOF
+#!/bin/sh
+exec tail -n +3 $0
+
+menuentry 'SystemRescueCd' {
+  load_video
+  insmod gzio
+  insmod part_gpt
+  insmod part_msdos
+  insmod ext2
+  search --no-floppy --fs-uuid $UUID --set=root --hint hd0,gpt1
+#  search --no-floppy --label boot --set=root
+  echo   'Loading Linux kernel ...'
+  linux  /sysresccd/vmlinuz archisobasedir=sysresccd archisolabel=boot copytoram setkmap=us
+  echo   'Loading initramfs ...'
+  initrd /sysresccd/sysresccd.img
+}
+EOF
