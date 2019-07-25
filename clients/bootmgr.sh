bae3a098b27b3940fbc905c73b77f367503cd11f
Dany Pinoy <dany.pinoy@pindanet.be>
2019-07-25 14:42:02 +0200

    Update bootmgr.sh

--- clients/bootmgr.sh
+++ clients/bootmgr.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # http://www.system-rescue-cd.org/manual/Installing_SystemRescueCd_on_the_disk/
+# Boot SystemRescueCd and copy system to RAM
 mkdir /mnt/cdrom
 mount /dev/sr0 /mnt/cdrom
 mkdir /mnt/esp
 mount /dev/sda1 /mnt/esp/
+cp -a /mnt/cdrom/sysresccd /mnt/esp/
