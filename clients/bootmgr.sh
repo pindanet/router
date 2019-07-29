72fd776b9ecf1b51f7ead41a8cb1ab53d01abe17
Dany Pinoy <dany.pinoy@pindanet.be>
2019-07-29 16:09:15 +0200

    Update bootmgr.sh

--- clients/bootmgr.sh
+++ clients/bootmgr.sh
@@ -6,25 +6,59 @@ mount /dev/sr0 /mnt/cdrom
 mkdir /mnt/esp
 mount /dev/sda1 /mnt/esp/
 cp -a /mnt/cdrom/sysresccd /mnt/esp/
+grub-install --target=x86_64-efi --efi-directory=/mnt/esp --boot-directory=/mnt/esp --bootloader-id=grub --recheck /dev/sda
 UUID=$(lsblk -o partlabel,uuid | grep "EFI system partition" | awk '{print $4}')
 DEVICE=$(blkid | grep $UUID | cut -d':' -f1)
-cat > /etc/grub.d/25_sysresccd <<EOF
-#!/bin/sh
-exec tail -n +3 $0
+cat > /mnt/esp/grub/grub.cfg <<EOF
+# Global options
+set timeout=20
+set default=0
+set fallback=1
+set pager=1
 
-menuentry 'SystemRescueCd' {
-  load_video
+# Display settings
+if loadfont /grub/fonts/unicode.pf2 ; then
+  set gfxmode=auto
+  insmod efi_gop
+  insmod efi_uga
+  insmod gfxterm
+  insmod videotest
+  insmod videoinfo
+  terminal_output gfxterm
+  insmod gfxmenu
+  insmod png
+  set theme=/grub/themes/starfield/theme.txt
+  export theme
+  set locale_dir=/grub/locale
+  set lang=nl
+  insmod gettext
+fi
+menuentry "Windows 10 starten" {
+  insmod part_gpt
+  insmod chain
+  set root='(hd0,gpt1)'
+  chainloader /efi/Microsoft/Boot/bootmgfw.efi 
+}
+menuentry "Windows 10 terugzetten" {
   insmod gzio
   insmod part_gpt
   insmod part_msdos
   insmod ext2
-  search --no-floppy --fs-uuid $UUID --set=root --hint hd0,gpt1
-#  search --no-floppy --label boot --set=root
+  set root='(hd0,gpt1)'
   echo   'Loading Linux kernel ...'
-  linux  /sysresccd/vmlinuz archisobasedir=sysresccd archisodevice=$DEVICE copytoram setkmap=be
+  linux  /sysresccd/boot/x86_64/vmlinuz archisobasedir=sysresccd archisodevice=/dev/sda1 copytoram setkmap=be restorewindows
   echo   'Loading initramfs ...'
-  initrd /sysresccd/sysresccd.img
+  initrd /sysresccd/boot/x86_64/sysresccd.img
+}
+menuentry 'SystemRescueCd (64bit)' {
+  insmod gzio
+  insmod part_gpt
+  insmod part_msdos
+  insmod ext2
+  set root='(hd0,gpt1)'
+  echo   'Loading Linux kernel ...'
+  linux  /sysresccd/boot/x86_64/vmlinuz archisobasedir=sysresccd archisodevice=/dev/sda1 copytoram setkmap=be
+  echo   'Loading initramfs ...'
+  initrd /sysresccd/boot/x86_64/sysresccd.img
 }
 EOF
-grub-install --target=x86_64-efi --efi-directory=/mnt/esp --boot-directory=/mnt/esp --bootloader-id=grub --recheck /dev/sda
-
