3219762ec4a37534f319c6571a16591f5fc3a599
Dany Pinoy <dany.pinoy@pindanet.be>
2019-07-31 16:29:46 +0200

    Update pindanet.sh

--- clients/pindanet.sh
+++ clients/pindanet.sh
@@ -2,57 +2,50 @@
 function fout {
 	printf "\n\033[1;31;40m %s. Computer NIET uitschakelen. Verwittig de systeembeheerder." "$1"
 	read line
-	exit
 }
 echo Welkom bij het PindaNet SNT Backupsysteem
 if [ "`grep restorewindows /proc/cmdline`" ]; then
-        mkdir /mnt/backup
-	mount /dev/sdb1 /mnt/backup/
-        gunzip -c /livemnt/boot/windows.img.gz | ntfsclone --restore-image --overwrite /dev/sda3 -
+        gunzip -c /mnt/backup/windows.img.gz | ntfsclone --restore-image --overwrite /dev/sda3 -
         if [ $? -ne 0 ]; then
         	fout "Herstellen Windows mislukt"
         fi
-	umount /mnt/backup/
-        shutdown -h
+        shutdown -h now
 fi
 setkmap be-latin1
 read -s -p "Backupwachtwoord: " wachtwoord
 echo
 if [ $wachtwoord == "sntlcvo" ]; then
-        mount /livemnt/boot -o rw,remount,relatime,data=ordered
-        rm /livemnt/boot/windows.parent.img.gz
-        mv /livemnt/boot/windows.img.gz /livemnt/boot/windows.parent.img.gz 
-        ntfsclone --save-image -o - /dev/sda4 | gzip -c > /livemnt/boot/windows.img.gz
+        rm /mnt/backup/windows.parent.img.gz
+        mv /mnt/backup/windows.img.gz /mnt/backup/windows.parent.img.gz 
+        ntfsclone --save-image -o - /dev/sda3 | gzip -c > /mnt/backup/windows.img.gz
         if [ $? -ne 0 ]; then
         	fout "Windows back-up aanmaken mislukt"
         fi
         shutdown -h now
 fi
 if [ $wachtwoord == "basis" ]; then
-        mount /livemnt/boot -o rw,remount,relatime,data=ordered
-        ntfsclone --save-image -o - /dev/sda4 | gzip -c > /livemnt/boot/windows.basis.img.gz
+        ntfsclone --save-image -o - /dev/sda3 | gzip -c > /mnt/backup/windows.basis.img.gz
         if [ $? -ne 0 ]; then
         	fout "Windows back-up aanmaken mislukt"
         fi
         shutdown -h now
 fi
 if [ $wachtwoord == "herstel" ]; then
-        gunzip -c /livemnt/boot/windows.basis.img.gz | ntfsclone --restore-image --overwrite /dev/sda4 -
+        gunzip -c /mnt/backup/windows.basis.img.gz | ntfsclone --restore-image --overwrite /dev/sda3 -
         if [ $? -ne 0 ]; then
         	fout "Herstellen Windows mislukt"
         fi
         shutdown -h now
 fi
 if [ $wachtwoord == "parent" ]; then
-        gunzip -c /livemnt/boot/windows.parent.img.gz | ntfsclone --restore-image --overwrite /dev/sda4 -
+        gunzip -c /mnt/backup/windows.parent.img.gz | ntfsclone --restore-image --overwrite /dev/sda3 -
         if [ $? -ne 0 ]; then
         	fout "Herstellen Windows mislukt"
         fi
         shutdown -h now
 fi
-if [ $wachtwoord == "sysrec" ]; then
-	exit
+if [ $wachtwoord != "sysrec" ]; then
+        printf '\033[1;31;40m Foutief Backupwachtwoord. Computer wordt binnen de minuut afgesloten.'
+        sleep 60
+        shutdown -h now
 fi
-printf '\033[1;31;40m Foutief Backupwachtwoord. Computer wordt binnen de minuut afgesloten.'
-sleep 60
-shutdown -h now
