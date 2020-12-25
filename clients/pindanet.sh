478f754f20a558d45869edbcd0526eac5d8fae80
Dany Pinoy <dany.pinoy@pindanet.be>
2020-12-25 18:52:43 +0100

    Update pindanet.sh

--- clients/pindanet.sh
+++ clients/pindanet.sh
@@ -5,7 +5,8 @@ function fout {
 }
 echo Welkom bij het PindaNet SNT Backupsysteem
 if [ "`grep restorewindows /proc/cmdline`" ]; then
-        gunzip -c /mnt/backup/windows.img.gz | ntfsclone --restore-image --overwrite /dev/sda3 -
+        #gunzip -c /mnt/backup/windows.img.gz | ntfsclone --restore-image --overwrite /dev/sda3 -
+        gunzip -c /mnt/backup/windows.img.gz | ntfsclone --restore-image --overwrite /dev/nvme0n1p3 -
         if [ $? -ne 0 ]; then
         	fout "Herstellen Windows mislukt"
         fi
@@ -17,28 +18,32 @@ echo
 if [ $wachtwoord == "sntlcvo" ]; then
         rm /mnt/backup/windows.parent.img.gz
         mv /mnt/backup/windows.img.gz /mnt/backup/windows.parent.img.gz 
-        ntfsclone --save-image -o - /dev/sda3 | gzip -c > /mnt/backup/windows.img.gz
+        #ntfsclone --save-image -o - /dev/sda3 | gzip -c > /mnt/backup/windows.img.gz
+	ntfsclone --save-image -o - /dev/nvme0n1p3 | gzip -c > /mnt/backup/windows.img.gz
         if [ $? -ne 0 ]; then
         	fout "Windows back-up aanmaken mislukt"
         fi
         shutdown -h now
 fi
 if [ $wachtwoord == "basis" ]; then
-        ntfsclone --save-image -o - /dev/sda3 | gzip -c > /mnt/backup/windows.basis.img.gz
+        #ntfsclone --save-image -o - /dev/sda3 | gzip -c > /mnt/backup/windows.basis.img.gz
+	ntfsclone --save-image -o - /dev/nvme0n1p3 | gzip -c > /mnt/backup/windows.basis.img.gz
         if [ $? -ne 0 ]; then
         	fout "Windows back-up aanmaken mislukt"
         fi
         shutdown -h now
 fi
 if [ $wachtwoord == "herstel" ]; then
-        gunzip -c /mnt/backup/windows.basis.img.gz | ntfsclone --restore-image --overwrite /dev/sda3 -
+        #gunzip -c /mnt/backup/windows.basis.img.gz | ntfsclone --restore-image --overwrite /dev/sda3 -
+	gunzip -c /mnt/backup/windows.basis.img.gz | ntfsclone --restore-image --overwrite /dev/nvme0n1p3 -
         if [ $? -ne 0 ]; then
         	fout "Herstellen Windows mislukt"
         fi
         shutdown -h now
 fi
 if [ $wachtwoord == "parent" ]; then
-        gunzip -c /mnt/backup/windows.parent.img.gz | ntfsclone --restore-image --overwrite /dev/sda3 -
+        #gunzip -c /mnt/backup/windows.parent.img.gz | ntfsclone --restore-image --overwrite /dev/sda3 -
+	gunzip -c /mnt/backup/windows.parent.img.gz | ntfsclone --restore-image --overwrite /dev/nvme0n1p3 -
         if [ $? -ne 0 ]; then
         	fout "Herstellen Windows mislukt"
         fi
